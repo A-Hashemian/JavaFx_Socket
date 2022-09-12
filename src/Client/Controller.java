b4e3aa58a90cd4d4fb0f98894b0162422773c8c7
A-Hashemian <amin@hashemian.tech>
2022-09-12 15:41:39 +0300

    up_03

--- src/Client/Controller.java
+++ src/Client/Controller.java
@@ -12,4 +12,48 @@ public class Controller {
     public static String username, password, gender;
     public static ArrayList<User> loggedInUser = new ArrayList<>();
     public static ArrayList<User> users = new ArrayList<User>();
+
+    public void registration() {
+        if (!regName.getText().equalsIgnoreCase("")
+                && !regPass.getText().equalsIgnoreCase("")
+                && !regEmail.getText().equalsIgnoreCase("")
+                && !regFirstName.getText().equalsIgnoreCase("")
+                && !regPhoneNo.getText().equalsIgnoreCase("")
+                && (male.isSelected() || female.isSelected())) {
+            if(checkUser(regName.getText())) {
+                if(checkEmail(regEmail.getText())) {
+                    User newUser = new User();
+                    newUser.name = regName.getText();
+                    newUser.password = regPass.getText();
+                    newUser.email = regEmail.getText();
+                    newUser.fullName = regFirstName.getText();
+                    newUser.phoneNo = regPhoneNo.getText();
+                    if (male.isSelected()) {
+                        newUser.gender = "Male";
+                    } else {
+                        newUser.gender = "Female";
+                    }
+                    users.add(newUser);
+                    goBack.setOpacity(1);
+                    success.setOpacity(1);
+                    makeDefault();
+                    if (controlRegLabel.getOpacity() == 1) {
+                        controlRegLabel.setOpacity(0);
+                    }
+                    if (nameExists.getOpacity() == 1) {
+                        nameExists.setOpacity(0);
+                    }
+                } else {
+                    checkEmail.setOpacity(1);
+                    setOpacity(nameExists, goBack, controlRegLabel, success);
+                }
+            } else {
+                nameExists.setOpacity(1);
+                setOpacity(success, goBack, controlRegLabel, checkEmail);
+            }
+        } else {
+            controlRegLabel.setOpacity(1);
+            setOpacity(success, goBack, nameExists, checkEmail);
+        }
+    }
 }
