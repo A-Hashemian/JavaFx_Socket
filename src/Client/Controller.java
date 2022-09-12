48b186b537070032369f33f267a444b3ffdbb258
A-Hashemian <amin@hashemian.tech>
2022-09-12 16:25:01 +0300

    up_10

--- src/Client/Controller.java
+++ src/Client/Controller.java
@@ -1,7 +1,12 @@
 package Client;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Controller {
@@ -119,6 +124,22 @@ public class Controller {
             changeWindow();
         } else {
             loginNotifier.setOpacity(1);
+        }
+    }
+
+    public void changeWindow() {
+        try {
+            Stage stage = (Stage) userName.getScene().getWindow();
+            Parent root = FXMLLoader.load(this.getClass().getResource("Room.fxml"));
+            stage.setScene(new Scene(root, 330, 560));
+            stage.setTitle(username + "");
+            stage.setOnCloseRequest(event -> {
+                System.exit(0);
+            });
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
