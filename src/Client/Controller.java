cac495d8bf5b5aa0cfe9ee7a82f47ab3f961f21d
A-Hashemian <amin@hashemian.tech>
2022-09-12 15:40:24 +0300

    up_02

---
+++ src/Client/Controller.java
@@ -0,0 +1,15 @@
+package Client;
+
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+
+public class Controller {
+
+
+
+    public Label checkEmail;
+    public static String username, password, gender;
+    public static ArrayList<User> loggedInUser = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<User>();
+}
