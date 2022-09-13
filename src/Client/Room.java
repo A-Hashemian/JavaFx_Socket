bbec4fa8f741a787551a693e991846e72e847ec6
A-Hashemian <amin@hashemian.tech>
2022-09-13 12:54:19 +0300

    up_17

--- src/Client/Room.java
+++ src/Client/Room.java
@@ -1,6 +1,58 @@
 package Client;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Room  extends Thread implements Initializable {
+
+    @FXML
+    public Label clientName;
+    @FXML
+    public Button chatBtn;
+    @FXML
+    public Pane chat;
+    @FXML
+    public TextField msgField;
+    @FXML
+    public TextArea msgRoom;
+    @FXML
+    public Label online;
+    @FXML
+    public Label fullName;
+    @FXML
+    public Label email;
+    @FXML
+    public Label phoneNo;
+    @FXML
+    public Label gender;
+    @FXML
+    public Pane profile;
+    @FXML
+    public Button profileBtn;
+    @FXML
+    public TextField fileChoosePath;
+    @FXML
+    public ImageView proImage;
+    @FXML
+    public Circle showProPic;
+    private FileChooser fileChooser;
+    private File filePath;
+    public boolean toggleChat = false, toggleProfile = false;
+
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket socket;
 }
