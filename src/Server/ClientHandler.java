c62f41c6d2489123011eb819500890fcfae46e7b
A-Hashemian <amin@hashemian.tech>
2022-09-20 23:18:41 +0300

    ClientHandler

---
+++ src/Server/ClientHandler.java
@@ -0,0 +1,32 @@
+package Server;
+
+import Client.Controller;
+import Client.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler extends Thread {
+
+    private ArrayList<ClientHandler> clients;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public ClientHandler(Socket socket, ArrayList<ClientHandler> clients) {
+        try {
+            this.socket = socket;
+            this.clients = clients;
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+  
+}
